cd26996e3741b9117b6723225268ef158b7068cb
Mananas <mananas@iMac-de-CAS-08.local>
2025-09-29 11:29:13 +0200

    IP haciendo la pantalla de resultado de zodiaccompatibility

--- app/src/main/java/com/example/horoscopo_android/Activities/ZodiacCompatibilityActivity.kt
+++ app/src/main/java/com/example/horoscopo_android/Activities/ZodiacCompatibilityActivity.kt
@@ -4,12 +4,22 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.example.horoscopo_android.Data.Horoscopo
 import com.example.horoscopo_android.MainActivity
 import com.example.horoscopo_android.R
 
 class ZodiacCompatibilityActivity : AppCompatActivity() {
+
+    private lateinit var spZodiac1: Spinner
+    private lateinit var spZodiac2: Spinner
+    private lateinit var btnNext: Button
+
+    private lateinit var horoscopo: List<Horoscopo>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_zodiac_compatibility)
@@ -18,8 +28,33 @@ class ZodiacCompatibilityActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.title = "Lazos Zodiacales"
 
-        /*  val imageView = findViewById<ImageView>(R.id.imageCompatibility)
-          imageView.setImageResource(R.drawable.imagen_compatibilidad_zodiacal)*/
+        spZodiac1 = findViewById<Spinner>(R.id.spZodiac1)
+        spZodiac2 = findViewById<Spinner>(R.id.spZodiac2)
+        btnNext = findViewById(R.id.btnNext)
+
+        horoscopo = Horoscopo.getAll()
+
+        val nombreSigno = horoscopo.map { getString(it.name) }
+
+        val adapter: ArrayAdapter<String> = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item,
+            nombreSigno
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spZodiac1.adapter = adapter
+        spZodiac2.adapter = adapter
+
+        btnNext.setOnClickListener {
+            val signo1 = horoscopo[spZodiac1.selectedItemPosition]
+            val signo2 = horoscopo[spZodiac2.selectedItemPosition]
+
+            val intent = Intent(this, ResultZodiacCompatibilityActivity::class.java)
+            intent.putExtra("SIGNO_1", signo1.id)
+            intent.putExtra("SIGNO_2", signo2.id)
+            startActivity(intent)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
